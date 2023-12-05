9037f82501fa23430e5ff0322d2e81461b9aae59
omkarfirame <omkarfirame@gmail.com>
2023-12-05 20:18:27 +0530

    Data ingestion pipeline created

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from textSumarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSumarizer.logging import logger
 
-logger.info("Welcome to custom logging")
+STAGE_NAME = "Data Ingestion Pipeline"
+
+try:
+    logger.info(f"----------{STAGE_NAME} Started !!----------")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f"----------{STAGE_NAME} Ended !!----------")
+except Exception as e:
+    logger.exception(e)
+    raise e
