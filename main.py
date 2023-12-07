e39d85cbfba7d8eab58d50a413d3a2aaa6b45127
omkarfirame <omkarfirame@gmail.com>
2023-12-07 20:33:06 +0530

    Data Validation Pipeline added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from textSumarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSumarizer.pipeline.stage_02_data_ingestion import DataValidationPipeline
 from textSumarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Pipeline"
@@ -7,6 +8,17 @@ try:
     logger.info(f"----------{STAGE_NAME} Started !!----------")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f"----------{STAGE_NAME} Ended !!----------")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Pipeline"
+
+try:
+    logger.info(f"----------{STAGE_NAME} Started !!----------")
+    data_validation = DataValidationPipeline()
+    data_validation.main()
     logger.info(f"----------{STAGE_NAME} Ended !!----------")
 except Exception as e:
     logger.exception(e)
---
+++ src/textSumarizer/pipeline/stage_02_data_ingestion.py
@@ -0,0 +1,14 @@
+from textSumarizer.config.configuration import ConfigurationManager
+from textSumarizer.components.data_validation import DataValidation
+from textSumarizer.logging import logger
+
+
+class DataValidationPipeline:
+    def __init__(self):
+        pass
+    
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_ingestion_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_files_exist()
