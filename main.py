b53ddf4cc827ce35ba11678a584db689d96a9ef8
omkarfirame <omkarfirame@gmail.com>
2023-12-18 20:38:12 +0530

    Data Transformation Pipeline added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textSumarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from textSumarizer.pipeline.stage_02_data_ingestion import DataValidationPipeline
+from textSumarizer.pipeline.stage_02_data_validation import DataValidationPipeline
+from textSumarizer.pipeline.stage_03_data_transformation import DataTransformationPipeline
 from textSumarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Pipeline"
@@ -23,3 +24,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Data Transformation Pipeline"
+
+try:
+    logger.info(f"----------{STAGE_NAME} Started !!----------")
+    data_transformation = DataTransformationPipeline()
+    data_transformation.main()
+    logger.info(f"----------{STAGE_NAME} Ended !!----------")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
